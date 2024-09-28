1b852155287f8385b63b948370a6e83d5cb93db0
Daniil Kalamin <56044849+dakalamin@users.noreply.github.com>
2024-09-28 11:41:10 +0300

    Add pio config file and baud_rate priority definition

--- src/config.h
+++ src/config.h
@@ -4,19 +4,19 @@
 
 
 /*
-   Normalized UTF-8 char (nUTF-8) - left padded with 0s to reach 32 bits
-   e.g.: "⠳" (U+2833) - E2 A0 B3 (hex UTF-8) - 00 E2 A0 B3 (hex nUTF-8)
+	Normalized UTF-8 char (nUTF-8) - left padded with 0s to reach 32 bits
+	e.g.: "⠳" (U+2833) - E2 A0 B3 (hex UTF-8) - 00 E2 A0 B3 (hex nUTF-8)
 */
 typedef uint32_t nchar32_t;
 
 /*
-   Braille 6-dot pattern:
-   (1)(4)
-   (2)(5) ⟶ 0bXX(6)(5)(4)(3)(2)(1)
-   (3)(6)
-   'X's are 'whatever' bits, additional information can be coded here
+	Braille 6-dot pattern:
+	(1)(4)
+	(2)(5) ⟶ 0bXX(6)(5)(4)(3)(2)(1)
+	(3)(6)
+	'X's are 'whatever' bits, additional information can be coded here
 
-   e.g.: "⠫" ⟶ (1)(2)(4)(6) ⟶ 0b101011
+	e.g.: "⠫" ⟶ (1)(2)(4)(6) ⟶ 0b101011
 */
 typedef uint8_t braille_t;
 
@@ -26,7 +26,9 @@ typedef uint8_t braille_t;
 #define latchPin  12  // ST_CP pin of 74HC595 shift register
 #define clockPin  13  // SH_CP pin of 74HC595 shift register
 
-#define BAUD_RATE 9600
+#ifndef BAUD_RATE
+	#define BAUD_RATE 9600
+#endif
 
 #define MIN_DELAY_MS 2
 
@@ -35,20 +37,20 @@ const byte BRAILLE_CELLS = 4;
 
 
 /*
-   LF is End Of Message
-   If 0, concatinates messages before and after LF symbol
+	LF is End Of Message
+	If 0, concatinates messages before and after LF symbol
 
-   e.g.: message is "x1[LF]0"
-   if 0 ⟶ "x10" ⟶ ⠭⠼⠁⠚   
-   if 1 ⟶ "x1"|"0" ⟶ ⠭⠼⠁|⠼⠚ 
+	e.g.: message is "x1[LF]0"
+	if 0 ⟶ "x10" ⟶ ⠭⠼⠁⠚   
+	if 1 ⟶ "x1"|"0" ⟶ ⠭⠼⠁|⠼⠚ 
 */
 #define LF_IS_EOM 0
 /*
-   Serial Not Available is End Of Message
-   If 0, concatinates messages before and after Serial not available
+	Serial Not Available is End Of Message
+	If 0, concatinates messages before and after Serial not available
 
-   e.g.: message is "x1[Serial NA]0"
-   if 0 ⟶ "x10" ⟶ ⠭⠼⠁⠚   
-   if 1 ⟶ "x1"|"0" ⟶ ⠭⠼⠁|⠼⠚ 
+	e.g.: message is "x1[Serial NA]0"
+	if 0 ⟶ "x10" ⟶ ⠭⠼⠁⠚   
+	if 1 ⟶ "x1"|"0" ⟶ ⠭⠼⠁|⠼⠚ 
 */
 #define SERIAL_NA_IS_EOM 0
